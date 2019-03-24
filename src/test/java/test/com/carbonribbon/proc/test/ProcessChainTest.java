bee3ff3dd99d19c70dbaa83de46d1d43d45a9bad
Patrik Dallmann <patrik.dallmann@gmail.com>
2019-03-24 21:45:42 +0100

    ProcessLink and ProcessChain unit tests

---
+++ src/test/java/test/com/carbonribbon/proc/test/ProcessChainTest.java
@@ -0,0 +1,88 @@
+package test.com.carbonribbon.proc.test;
+
+import com.carbonribbon.proc.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import test.com.carbonribbon.proc.test.impl.StringIndentProcessLink;
+import test.com.carbonribbon.proc.test.impl.StringPadProcessLink;
+
+/**
+ * Unit tests for {@link ProcessChain}
+ */
+public class ProcessChainTest {
+
+    private static final String TEST_STRING = "TEST";
+    private static final String INDENT_EXPECTED = " TEST";
+    private static final String INDENT_EXPECTED2 = "  TEST";
+    private static final String INDENT_PAD_EXPECTED = "+ TEST";
+    private static final String OTHER_EXPECTED = "+TEST";
+    private static final String PAD_STRING = "+";
+
+    private ProcessLinkFactory extendedDefaultLinkFactory;
+
+    @Before
+    public void setup() {
+        extendedDefaultLinkFactory = new DefaultProcessLinkFactory() {
+            @Override
+            public ProcessLink newProcessLink(Class<? extends ProcessLink> clazz) {
+                if (StringPadProcessLink.class.equals(clazz)) {
+                    return new StringPadProcessLink("+");
+                }
+                return super.newProcessLink(clazz);
+            }
+        };
+    }
+
+    @Test
+    public void testChainWithZeroLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        Assert.assertEquals(chain.applyChain(TEST_STRING), TEST_STRING);
+    }
+
+    @Test
+    public void testChainWithOneLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        chain.addLink(StringIndentProcessLink.class);
+        Assert.assertEquals(chain.applyChain(TEST_STRING), INDENT_EXPECTED);
+    }
+
+    @Test
+    public void testChainWithTwoLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        chain.addLink(StringIndentProcessLink.class);
+        chain.addLink(StringIndentProcessLink.class);
+        Assert.assertEquals(chain.applyChain(TEST_STRING), INDENT_EXPECTED2);
+    }
+
+    @Test
+    public void testChainWithTwoDifferentLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        chain.addLink(StringIndentProcessLink.class);
+        chain.addLink(new StringPadProcessLink(PAD_STRING));
+        Assert.assertEquals(chain.applyChain(TEST_STRING), INDENT_PAD_EXPECTED);
+    }
+
+    @Test
+    public void testChainWithTwoDifferentLinksCustomFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>(extendedDefaultLinkFactory);
+        chain.addLink(StringIndentProcessLink.class);
+        chain.addLink(StringPadProcessLink.class);
+        Assert.assertEquals(chain.applyChain(TEST_STRING), INDENT_PAD_EXPECTED);
+    }
+
+    @Test
+    public void testChainWithOneConditionalNegativeLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        chain.addLink(StringIndentProcessLink.class, s -> !TEST_STRING.equals(s));
+        Assert.assertEquals(chain.applyChain(TEST_STRING), TEST_STRING);
+    }
+
+    @Test
+    public void testChainWithOneConditionalPositiveLinksDefaultFactory() {
+        ProcessChain<String> chain = new LinkedProcessChain<>();
+        chain.addLink(StringIndentProcessLink.class, TEST_STRING::equals);
+        Assert.assertEquals(chain.applyChain(TEST_STRING), INDENT_EXPECTED);
+    }
+
+}
---
+++ src/test/java/test/com/carbonribbon/proc/test/ProcessLinkTest.java
@@ -0,0 +1,43 @@
+package test.com.carbonribbon.proc.test;
+
+import com.carbonribbon.proc.ProcessLink;
+import org.junit.Assert;
+import org.junit.Test;
+import test.com.carbonribbon.proc.test.impl.StringIndentProcessLink;
+import test.com.carbonribbon.proc.test.impl.StringPadProcessLink;
+
+/**
+ * Unit tests for {@link ProcessLink}
+ */
+public class ProcessLinkTest {
+
+    private static final String TEST_STRING = "TEST";
+    private static final String INDENT_EXPECTED = " TEST";
+    private static final String INDENT_EXPECTED2 = "  TEST";
+    private static final String PAD_EXPECTED = "+TEST";
+    private static final String INDENT_PAD_EXPECTED = "+ TEST";
+    private static final String PAD_STRING = "+";
+
+    @Test
+    public void testProcessLinkApply() {
+        ProcessLink stringIndentProcessLink = new StringIndentProcessLink();
+        Assert.assertEquals(stringIndentProcessLink.apply(TEST_STRING), INDENT_EXPECTED);
+    }
+
+    @Test
+    public void testRepeatedProcessLinkApply() {
+        ProcessLink stringIndentProcessLink = new StringIndentProcessLink();
+        String ss = (String)stringIndentProcessLink.apply(TEST_STRING);
+        Assert.assertEquals(stringIndentProcessLink.apply(ss), INDENT_EXPECTED2);
+    }
+
+    @Test
+    public void testDifferentProcessLinkApply() {
+        ProcessLink stringIndentProcessLink = new StringIndentProcessLink();
+        ProcessLink stringPadProcessLink = new StringPadProcessLink(PAD_STRING);
+        String indentedString = (String) stringIndentProcessLink.apply(TEST_STRING);
+        Assert.assertEquals(indentedString, INDENT_EXPECTED);
+        Assert.assertEquals(stringPadProcessLink.apply(TEST_STRING), PAD_EXPECTED);
+        Assert.assertEquals(stringPadProcessLink.apply(indentedString), INDENT_PAD_EXPECTED);
+    }
+}
